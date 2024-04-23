aeb6a0bfdb3a35b6821c5945129e3ac384245649
Брусенцев Александр Юрьевич <abrusentsev@aviatron.ru>
2024-04-23 17:16:48 +0500

    new baudrates were added

--- src/modbus-rtu.c
+++ src/modbus-rtu.c
@@ -537,76 +537,51 @@ static speed_t _get_termios_speed(int baud, int debug)
     case 38400:
         speed = B38400;
         break;
-#ifdef B57600
     case 57600:
         speed = B57600;
         break;
-#endif
-#ifdef B115200
     case 115200:
         speed = B115200;
         break;
-#endif
-#ifdef B230400
     case 230400:
         speed = B230400;
         break;
-#endif
-#ifdef B460800
     case 460800:
         speed = B460800;
         break;
-#endif
-#ifdef B500000
     case 500000:
         speed = B500000;
         break;
-#endif
-#ifdef B576000
     case 576000:
         speed = B576000;
         break;
-#endif
-#ifdef B921600
     case 921600:
         speed = B921600;
         break;
-#endif
-#ifdef B1000000
     case 1000000:
         speed = B1000000;
         break;
-#endif
-#ifdef B1152000
     case 1152000:
         speed = B1152000;
         break;
-#endif
-#ifdef B1500000
     case 1500000:
         speed = B1500000;
         break;
-#endif
-#ifdef B2500000
+	case 1843200:
+        speed = B1843200;
+        break;
     case 2500000:
         speed = B2500000;
         break;
-#endif
-#ifdef B3000000
     case 3000000:
         speed = B3000000;
         break;
-#endif
-#ifdef B3500000
     case 3500000:
         speed = B3500000;
         break;
-#endif
-#ifdef B4000000
     case 4000000:
         speed = B4000000;
         break;
-#endif
     default:
         speed = B9600;
         if (debug) {
